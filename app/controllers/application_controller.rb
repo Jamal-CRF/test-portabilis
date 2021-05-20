696696123fda09779dc5728870dc141e4f40c386
lucas mouta <lucasaraujomouta@gmail.com>
2021-05-20 15:12:10 -0300

    ....

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  include Pundit
+
+
+private
+
+  def user_not_authorized
+    flash[:notice] = "Você não tem permissão para fazer esta ação :("
+    redirect_to(request.referrer || root_path) 
+  end
 end
